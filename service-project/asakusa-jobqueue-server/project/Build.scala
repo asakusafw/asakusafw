7416e5b5fe9e3f83a5f7226cf6af0038d7a85b4e
KAWAGUCHI, Akira <akirakw@gmail.com>
2013-07-26 18:43:15 +0900

    Bumped version number to 0.5.1

--- service-project/asakusa-jobqueue-server/project/Build.scala
+++ service-project/asakusa-jobqueue-server/project/Build.scala
@@ -9,7 +9,7 @@ import org.codehaus.plexus.archiver.tar.TarArchiver
 object JobQueueBuildSettings {
 
   val appName = "asakusa-jobqueue"
-  val appVersion = "0.5.1-RC3"
+  val appVersion = "0.5.1"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.asakusafw",
