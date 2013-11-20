032db9e6c42e74ee804b097dc441bfbadbeb5ddc
KAWAGUCHI, Akira <akirakw@gmail.com>
2013-11-20 11:22:27 +0900

    Bumped version number to 0.5.2-hadoop2

--- service-project/asakusa-jobqueue-server/project/Build.scala
+++ service-project/asakusa-jobqueue-server/project/Build.scala
@@ -9,7 +9,7 @@ import org.codehaus.plexus.archiver.tar.TarArchiver
 object JobQueueBuildSettings {
 
   val appName = "asakusa-jobqueue"
-  val appVersion = "0.5.1-hadoop2"
+  val appVersion = "0.5.2-hadoop2"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.asakusafw",
