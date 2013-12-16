3552400ba9d3e88e9cb94218217d3180baf83a89
KAWAGUCHI, Akira <akirakw@gmail.com>
2013-12-16 19:09:30 +0900

    Add groovydoc to Asakusa Gradle Plugin (#323).

--- distribution-project/asakusa-gradle-plugins/src/main/groovy/com/asakusafw/gradle/plugins/AsakusafwPlugin.groovy
+++ distribution-project/asakusa-gradle-plugins/src/main/groovy/com/asakusafw/gradle/plugins/AsakusafwPlugin.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011-2013 Asakusa Framework Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.asakusafw.gradle.plugins
 
 import org.gradle.api.*
@@ -6,6 +21,9 @@ import org.gradle.api.tasks.bundling.*
 
 import com.asakusafw.gradle.tasks.*
 
+/**
+ * Gradle plugin for building application component blocks.
+ */
 class AsakusafwPlugin implements Plugin<Project> {
 
     public static final String ASAKUSAFW_BUILD_GROUP = 'Asakusa Framework Build'
