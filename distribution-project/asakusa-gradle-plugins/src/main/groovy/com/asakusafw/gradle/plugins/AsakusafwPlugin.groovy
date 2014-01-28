b0aaddae5cc281afe9021c8bc3e407bb3b850e8e
KAWAGUCHI, Akira <akirakw@gmail.com>
2014-01-28 14:51:20 +0900

    Fixed #356.
    Task inputs/outputs property does not evaluate correctly when changing
    that [Gradle Plugin]

--- distribution-project/asakusa-gradle-plugins/src/main/groovy/com/asakusafw/gradle/plugins/AsakusafwPlugin.groovy
+++ distribution-project/asakusa-gradle-plugins/src/main/groovy/com/asakusafw/gradle/plugins/AsakusafwPlugin.groovy
@@ -122,8 +122,8 @@ class AsakusafwPlugin implements Plugin<Project> {
         project.task('compileDMDL', type: CompileDmdlTask) {
             group ASAKUSAFW_BUILD_GROUP
             description 'Compiles the DMDL scripts with DMDL Compiler.'
-            source project.asakusafw.dmdl.dmdlSourceDirectory
-            outputs.dir project.asakusafw.modelgen.modelgenSourceDirectory
+            source { project.asakusafw.dmdl.dmdlSourceDirectory }
+            outputs.dir { project.asakusafw.modelgen.modelgenSourceDirectory }
         }
     }
 
@@ -147,7 +147,7 @@ class AsakusafwPlugin implements Plugin<Project> {
         project.task('jarBatchapp', type: Jar, dependsOn: 'compileBatchapp') {
             group ASAKUSAFW_BUILD_GROUP
             description 'Assembles a jar archive containing compiled batch applications.'
-            from project.asakusafw.compiler.compiledSourceDirectory
+            from { project.asakusafw.compiler.compiledSourceDirectory }
             destinationDir project.buildDir
             appendix 'batchapps'
             onlyIf { dependsOnTaskDidWork() }
@@ -162,8 +162,8 @@ class AsakusafwPlugin implements Plugin<Project> {
         project.task('generateTestbook', type: GenerateTestbookTask) {
             group ASAKUSAFW_BUILD_GROUP
             description 'Generates the template Excel books for TestDriver.'
-            source project.asakusafw.dmdl.dmdlSourceDirectory
-            outputs.dir project.asakusafw.testtools.testDataSheetDirectory
+            source { project.asakusafw.dmdl.dmdlSourceDirectory }
+            outputs.dir { project.asakusafw.testtools.testDataSheetDirectory }
         }
     }
 
