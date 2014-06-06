9f880876116b81910f6b2563b3a445b8e5d6127e
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2014-06-06 17:56:08 +0900

    Add ordinal comparison rule for Excel (#395).

---
+++ testing-project/asakusa-test-moderator/src/main/java/com/asakusafw/testdriver/rule/CompareOperator.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2011-2014 Asakusa Framework Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asakusafw.testdriver.rule;
+
+/**
+ * Represents a compare operator.
+ * @since 0.7.0
+ */
+public enum CompareOperator {
+
+    /**
+     * {@code <}.
+     */
+    LESS_THAN("<") {
+        @Override
+        public <T extends Comparable<T>> boolean satisfies(T a, T b) {
+            if (a == null || b == null) {
+                throw new IllegalArgumentException();
+            }
+            return a.compareTo(b) < 0;
+        }
+    },
+
+    /**
+     * {@code >}.
+     */
+    GREATER_THAN(">") {
+        @Override
+        public <T extends Comparable<T>> boolean satisfies(T a, T b) {
+            if (a == null || b == null) {
+                throw new IllegalArgumentException();
+            }
+            return a.compareTo(b) > 0;
+        }
+    },
+
+    /**
+     * {@code <=}.
+     */
+    LESS_THAN_OR_EQUAL("<=") {
+        @Override
+        public <T extends Comparable<T>> boolean satisfies(T a, T b) {
+            if (a == null || b == null) {
+                throw new IllegalArgumentException();
+            }
+            return a.compareTo(b) <= 0;
+        }
+    },
+
+    /**
+     * {@code >=}.
+     */
+    GREATER_THAN_OR_EQUAL(">=") {
+        @Override
+        public <T extends Comparable<T>> boolean satisfies(T a, T b) {
+            if (a == null || b == null) {
+                throw new IllegalArgumentException();
+            }
+            return a.compareTo(b) >= 0;
+        }
+    },
+    ;
+
+    private final String symbol;
+
+    private CompareOperator(String symbol) {
+        this.symbol = symbol;
+    }
+
+    /**
+     * Returns the symbol.
+     * @return the symbol
+     */
+    public String getSymbol() {
+        return symbol;
+    }
+
+    /**
+     * Returns whether {@code a <operator> b} is satisfied or not.
+     * @param <T> the data type
+     * @param a the first term
+     * @param b the second term
+     * @return {@code true} if it is satisfied, or {@code false} otherwise
+     */
+    public abstract <T extends Comparable<T>> boolean satisfies(T a, T b);
+}
