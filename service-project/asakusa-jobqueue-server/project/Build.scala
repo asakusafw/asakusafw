83d9bc216d19dcd1e1f1013535cfd4d36293c2d0
KAWAGUCHI, Akira <akirakw@gmail.com>
2014-06-12 14:12:24 +0900

    Bumped version number to 0.7-hadoop1-SNAPSHOT

--- service-project/asakusa-jobqueue-server/project/Build.scala
+++ service-project/asakusa-jobqueue-server/project/Build.scala
@@ -9,7 +9,7 @@ import org.codehaus.plexus.archiver.tar.TarArchiver
 object JobQueueBuildSettings {
 
   val appName = "asakusa-jobqueue"
-  val appVersion = "0.7-SNAPSHOT"
+  val appVersion = "0.7-hadoop1-SNAPSHOT"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.asakusafw",
