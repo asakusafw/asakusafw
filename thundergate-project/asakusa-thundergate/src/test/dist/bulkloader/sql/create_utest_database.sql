025cc6d61f079fa88a374fae54c4c6ec20a79ceb
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2016-01-21 15:45:49 +0900

    Move legacy projects to asakusafw-legacy repository
    The destination commit:
    asakusafw/asakusafw-legacy@6406556745f1ac9e9128e74ad8d8dbedc3d54fe9

--- thundergate-project/asakusa-thundergate/src/test/dist/bulkloader/sql/create_utest_database.sql
+++
@@ -1,3 +0,0 @@
-create database __asakusa_utest_thundergate;
-GRANT ALL PRIVILEGES ON *.* TO __asakusa_ut_tg IDENTIFIED BY '__asakusa_ut_tg' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO __asakusa_ut_tg@"localhost" IDENTIFIED BY "__asakusa_ut_tg" WITH GRANT OPTION;
