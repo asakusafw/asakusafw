431c7b5f8b4bebb051238354d5a656e7bb07ae3f
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2019-05-16 16:13:53 +0900

    fix tracking input file size of Orc/Parquet.

--- hive-project/asakusa-hive-core/src/main/java/com/asakusafw/directio/hive/orc/OrcFileInput.java
+++ hive-project/asakusa-hive-core/src/main/java/com/asakusafw/directio/hive/orc/OrcFileInput.java
@@ -66,6 +66,8 @@ public class OrcFileInput<T> implements ModelInput<T> {
 
     private Object structBuffer;
 
+    private long lastCount;
+
     /**
      * Creates a new instance.
      * @param descriptor the target data model descriptor
@@ -118,8 +120,7 @@ public class OrcFileInput<T> implements ModelInput<T> {
         driver.set(model, buf);
         structBuffer = buf;
 
-        // NOTE: only tell this is alive
-        counter.add(0);
+        advanceCounter((long) (fragmentSize * reader.getProgress()));
         return true;
     }
 
@@ -172,12 +173,18 @@ public class OrcFileInput<T> implements ModelInput<T> {
         return availables;
     }
 
+    private void advanceCounter(long nextCount) {
+        long deltaCount = nextCount - lastCount;
+        if (deltaCount > 0) {
+            counter.add(deltaCount);
+            lastCount = nextCount;
+        }
+    }
+
     @Override
     public void close() throws IOException {
         if (currentReader != null) {
             currentReader.close();
-            currentReader = null;
-            counter.add(Util.getFileSize(path, fileSystem));
         }
     }
 }
